acd9eaa6d09203681bd70c467df9342da1fe22b9
Kevin Schoon <kevinschoon@gmail.com>
2018-05-26 15:10:35 +0100

    add more test client test cases

--- client_test.go
+++ client_test.go
@@ -20,7 +20,47 @@ var mockResp = []byte(`
 }
 `)
 
-var mockError = []byte(`
+var mockCancellationResp = []byte(`
+{
+  "controller_id": "example_controller_id",
+  "subject_request_id": "a7551968-d5d6-44b2-9831-815ac9017798",
+  "received_time": "2018-10-02T15:00:01Z",
+  "api_version": "1.0"
+}
+`)
+
+var mockStatusResp = []byte(`
+{
+    "controller_id":"example_controller_id",
+    "expected_completion_time":"2018-11-01T15:00:01Z",
+    "subject_request_id":"a7551968-d5d6-44b2-9831-815ac9017798",
+    "request_status":"pending",
+    "api_version":"1.0",
+    "results_url":"https://exampleprocessor.com/secure/d188d4ba-12db-48a0-898c-cd0f8ba7b345"
+}
+`)
+
+var mockDiscoveryResp = []byte(`
+{
+   "api_version":"1.0",
+   "supported_identities":[
+      {
+         "identity_type":"email",
+         "identity_format":"raw"
+      },
+      {
+         "identity_type":"email",
+         "identity_format":"sha256"
+      }
+   ],
+   "supported_subject_request_types":[
+      "erasure"
+   ],
+   "processor_certificate":"https://exampleprocessor.com/cert.pem"
+}
+`)
+
+var mockErrorResp = []byte(`
 {
   "error": {
     "code": 400,
@@ -50,7 +90,7 @@ func newMockCaller(resp *http.Response, err error) *mockCaller {
 }
 
 func TestClientRequest(t *testing.T) {
-	c := NewClient(&ClientOptions{Endpoint: "http://mock-endpoint"})
+	c := NewClient(&ClientOptions{})
 	c.caller = newMockCaller(&http.Response{
 		StatusCode: 200,
 		Body:       ioutil.NopCloser(bytes.NewBuffer(mockResp)),
@@ -61,13 +101,57 @@ func TestClientRequest(t *testing.T) {
 	assert.Equal(t, "a7551968-d5d6-44b2-9831-815ac9017798", resp.SubjectRequestId)
 }
 
-func TestClientError(t *testing.T) {
-	c := NewClient(&ClientOptions{Endpoint: "http://mock-endpoint"})
+func TestClientStatus(t *testing.T) {
+	c := NewClient(&ClientOptions{})
 	c.caller = newMockCaller(&http.Response{
-		StatusCode: 400,
-		Body:       ioutil.NopCloser(bytes.NewBuffer(mockError)),
+		StatusCode: 200,
+		Body:       ioutil.NopCloser(bytes.NewBuffer(mockStatusResp)),
+	}, nil)
+	resp, err := c.Status("1234")
+	assert.NoError(t, err)
+	assert.Equal(t, "a7551968-d5d6-44b2-9831-815ac9017798", resp.SubjectRequestId)
+	assert.Equal(t, "example_controller_id", resp.ControllerId)
+	assert.Equal(t, "1.0", resp.ApiVersion)
+	assert.Equal(t, STATUS_PENDING, resp.RequestStatus)
+}
+
+func TestClientCancel(t *testing.T) {
+	c := NewClient(&ClientOptions{})
+	c.caller = newMockCaller(&http.Response{
+		StatusCode: 200,
+		Body:       ioutil.NopCloser(bytes.NewBuffer(mockCancellationResp)),
+	}, nil)
+	resp, err := c.Cancel("1234")
+	assert.NoError(t, err)
+	assert.Equal(t, "a7551968-d5d6-44b2-9831-815ac9017798", resp.SubjectRequestId)
+	assert.Equal(t, "example_controller_id", resp.ControllerId)
+	assert.Equal(t, "1.0", resp.ApiVersion)
+}
+
+func TestClientDiscover(t *testing.T) {
+	c := NewClient(&ClientOptions{})
+	c.caller = newMockCaller(&http.Response{
+		StatusCode: 200,
+		Body:       ioutil.NopCloser(bytes.NewBuffer(mockDiscoveryResp)),
+	}, nil)
+	resp, err := c.Discovery()
+	assert.NoError(t, err)
+	assert.Equal(t, IDENTITY_EMAIL, resp.SupportedIdentities[0].Type)
+	assert.Equal(t, FORMAT_RAW, resp.SupportedIdentities[0].Format)
+	assert.Equal(t, IDENTITY_EMAIL, resp.SupportedIdentities[1].Type)
+	assert.Equal(t, FORMAT_SHA256, resp.SupportedIdentities[1].Format)
+	assert.Equal(t, SUBJECT_ERASURE, resp.SupportedSubjectRequestTypes[0])
+}
+
+func TestClientError(t *testing.T) {
+	c := NewClient(&ClientOptions{})
+	c.caller = newMockCaller(&http.Response{
+		StatusCode: 500,
+		Body:       ioutil.NopCloser(bytes.NewBuffer(mockErrorResp)),
 	}, nil)
 	_, err := c.Request(&Request{})
 	assert.Error(t, err)
 	assert.IsType(t, &ErrorResponse{}, err)
+	assert.Equal(t, "IllegalArgumentException", err.(*ErrorResponse).Errors[0].Reason)
+	assert.Equal(t, "subject_request_id field is required.", err.(*ErrorResponse).Errors[0].Message)
 }
