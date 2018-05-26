71f6dbe104f51ab0ff20330f82e589991ba3a063
Kevin Schoon <kevinschoon@gmail.com>
2018-05-26 15:37:58 +0100

    update utils

--- utils.go
+++ utils.go
@@ -2,7 +2,7 @@ package gdpr
 
 // SupportedFunc returns a function that checks if the server can
 // support a specific request.
-func SupportedFunc(opts *ServerOptions) func(*Request) bool {
+func SupportedFunc(opts *ServerOptions) func(*Request) error {
 	subjectMap := map[SubjectType]bool{}
 	for _, subjectType := range opts.SubjectTypes {
 		subjectMap[subjectType] = true
@@ -11,15 +11,28 @@ func SupportedFunc(opts *ServerOptions) func(*Request) bool {
 	for _, identity := range opts.Identities {
 		identityMap[string(identity.Type)+string(identity.Format)] = true
 	}
-	return func(req *Request) bool {
+	return func(req *Request) error {
 		if _, ok := subjectMap[req.SubjectRequestType]; !ok {
-			return false
+			return ErrUnsupportedRequestType(req.SubjectRequestType)
 		}
 		for _, identity := range req.SubjectIdentities {
 			if _, ok := identityMap[string(identity.Type)+string(identity.Format)]; !ok {
-				return false
+				return ErrUnsupportedIdentity(identity)
 			}
 		}
-		return true
+		return nil
+	}
+}
+
+func ValidateRequest(opts *ServerOptions) func(*Request) error {
+	fn := SupportedFunc(opts)
+	return func(req *Request) error {
+		if req.SubjectRequestId == "" {
+			return ErrMissingRequiredField("subject_request_id")
+		}
+		if len(req.SubjectIdentities) == 0 {
+			return ErrMissingRequiredField("subject_identities")
+		}
+		return fn(req)
 	}
 }
